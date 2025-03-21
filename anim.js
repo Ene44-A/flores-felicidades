0176eba1c1ea4c8f061f99eadf6f67d181876a8a
Andrés García <79813523+Ene44-A@users.noreply.github.com>
2025-03-21 09:47:23 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -37,13 +37,13 @@ var lyricsData = [
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
   var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 6
+    (line) => time >= line.time && time < line.time + 10
   );
 
   if (currentLine) {
     // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
-    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+    var fadeInDuration = .5; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1.5, (time - currentLine.time) / fadeInDuration);
 
     // Aplica el efecto de aparición
     lyrics.style.opacity = opacity;
@@ -55,7 +55,7 @@ function updateLyrics() {
   }
 }
 
-setInterval(updateLyrics, 1000);
+setInterval(updateLyrics, 10000);
 
 //funcion titulo
 // Función para ocultar el título después de 216 segundos
